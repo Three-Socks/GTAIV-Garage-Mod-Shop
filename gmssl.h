82d16438f94124445c6d06a0d0f153dcaf98618e
ThreeSocks <ThreeSocks@localhost>
2012-05-05 01:38:09 +0100

    SaveLoad TEST2.

--- gmssl.h
+++ gmssl.h
@@ -18,3 +18,35 @@ typedef struct _drawVNeonS
 	float mrange;
 	float height;
 } drawVNeonS;
+
+typedef struct _saveVehicleS
+{
+	uint model_n;
+	ColourIndex normal_colour1;
+	ColourIndex normal_colour2;
+	ColourIndex extra_colour1;
+	ColourIndex extra_colour2;
+	uint upgrade1;
+	uint upgrade2;
+	uint upgrade3;
+	uint upgrade4;
+	uint upgrade5;
+	uint upgrade6;
+	uint upgrade7;
+	uint upgrade8;
+	uint upgrade9;
+	uint neon_toggle;
+	uint neon_togglefb;
+	uint neon_colour_r;
+	uint neon_colour_g;
+	uint neon_colour_b;
+	uint neon_colour_n;
+	float neon_fyoff;
+	float neon_byoff;
+	uint neon_fbintensity;
+	float neon_fbrange;
+	float neon_myoff;
+	uint neon_mintensity;
+	float neon_mrange;
+	float neon_height;
+} saveVehicleS;
